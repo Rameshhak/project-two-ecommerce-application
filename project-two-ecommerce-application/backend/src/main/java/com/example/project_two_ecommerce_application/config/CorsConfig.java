f1aa34acaa5fc9df4065b65e3c3acaffbad3a380
Rameshhak <rameshhari2511@gmail.com>
2025-10-08 21:46:51 +0530

    Update CorsConfig.java

--- project-two-ecommerce-application/backend/src/main/java/com/example/project_two_ecommerce_application/config/CorsConfig.java
+++ project-two-ecommerce-application/backend/src/main/java/com/example/project_two_ecommerce_application/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig{
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOrigins("http://localhost:5173")
+                            .allowedOrigins("https://frabjous-belekoy-4bc68d.netlify.app/")
                             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                             .allowedHeaders("*")
                             .exposedHeaders("Authorization")
@@ -24,4 +24,5 @@ public class CorsConfig{
             };
         }
 }
+
 
